a49440b7eabce1db9a51d20c0cdef71e6a13532e
epdlahtm92 <epdlahtm92@gmail.com>
2024-03-05 19:35:36 +0900

    Start Project

---
+++ src/main/java/com/weblib/gameslib/GameslibApplication.java
@@ -0,0 +1,13 @@
+package com.weblib.gameslib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameslibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameslibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weblib/gameslib/GameslibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weblib.gameslib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameslibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
